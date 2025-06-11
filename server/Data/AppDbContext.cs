a668072f95783b52e3cf4c44dda822fa69be3136
aeriel999 <ok4743@gmail.com>
2025-06-11 12:09:48 +0300

    Add new projeckts for clean architecture

---
+++ server/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using server.Models;
+
+namespace server.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Category> Categories { get; set; }
+
+    public DbSet<CategoryItem> CategoryItems { get; set; }
+
+    public DbSet<Expense> Expenses { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
